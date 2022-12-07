83e39e53239a2c9a59edb2286c7db158e77a575e
nicoli santana <nicoliapsantana@gmail.com>
2022-12-07 15:18:53 -0300

    v1.0

--- js/calculadora.js
+++ js/calculadora.js
@@ -26,9 +26,9 @@
     }
 
     function Bhaskara(){
-        a = document.getElementById("a").value;
-        b = document.getElementById("b").value;
-        c = document.getElementById("c").value;
+        a = document.getElementById("a1").value;
+        b = document.getElementById("b1").value;
+        c = document.getElementById("c1").value;
         delta = eval(perseInt(Math,pow(b, 2) - 4*a*c));
 
         x1 = eval(parseInt(( -b + Math.sqrt(delta)) /2*a));
@@ -39,32 +39,32 @@
       }
 
       function Bhaskara(){
-        a = document.getElementById("a").value;
-        b = document.getElementById("b").value;
-        c = document.getElementById("c").value;
+        a = document.getElementById("a1").value;
+        b = document.getElementById("b1").value;
+        c = document.getElementById("c1").value;
         
         delta = eval(parseFloat((b * b) -4 * a * c));
-        document.getElementById("resultado-bhaskara1").innerHTML = "delta = " + delta;
+        document.getElementById("resul-bha1").innerHTML = "delta = " + delta;
       
         if(delta < 0){
-        document.getElementById("resultado-bhaskara2").innerHTML = "O delta é negativo. Equação não possui raizes reais.";
+        document.getElementById("resul-bha2").innerHTML = "O delta é negativo. Equação não possui raizes reais.";
 
         }else if(delta == 0){
           x1 = eval(parseFloat((-b + Math.sqrt(delta)) / (2 * a)));
-          document.getElementById("resultado-bhaskara2").innerHTML = "X1 e X2 = " + x1;
+          document.getElementById("resul-bha2").innerHTML = "X1 e X2 = " + x1;
             
         }else{
           x1 = eval(parseFloat((-b + Math.sqrt(delta)) / (2 * a)));
           x2 = eval(parseFloat((-b - Math.sqrt(delta)) / (2 * a)));
-          document.getElementById("resultado-bhaskara2").innerHTML = "X1 = " + x1;
-          document.getElementById("resultado-bhaskara3").innerHTML = "X2 = " + x2;
+          document.getElementById("resul-bha2").innerHTML = "X1 = " + x1;
+          document.getElementById("resul-bha3").innerHTML = "X2 = " + x2;
           
           
         }
 
        
-        document.getElementById("resultado-bhaskara2").innerHTML = resultado1;
-        document.getElementById("resultado-bhaskara3").innerHTML = resultado2;
+        document.getElementById("resul-bha2").innerHTML = resultado1;
+        document.getElementById("resul-bha3").innerHTML = resultado2;
 
       }
    
